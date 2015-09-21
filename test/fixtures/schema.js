34ca9a191daf53763821f4f7198b1b0461883d11
Adam Bretz <arbretz@gmail.com>
2015-09-21 17:17:24 -0400

    PR updates 1.

--- test/fixtures/schema.js
+++ test/fixtures/schema.js
@@ -2,17 +2,17 @@ var Joi = require('joi');
 
 module.exports = Joi.object().keys({
   runtime: Joi.object({
-    argv: Joi.array().items(Joi.string()).sparse(),
+    argv: Joi.array().items(Joi.string()),
     cwd: Joi.string(),
     env: Joi.object().unknown(true).pattern(/.+/, Joi.string().allow('')),
     execPath: Joi.string(),
-    execArgv: Joi.array().items(Joi.string()).sparse(),
+    execArgv: Joi.array().items(Joi.string()),
     versions: Joi.object().unknown(true).pattern(/.+/, Joi.string().allow(''))
   }),
   connections: Joi.array().items(Joi.object({
     uri: Joi.string(),
-    labels: Joi.array().items(Joi.string().allow('')).sparse(),
-    routes: Joi.array().items(Joi.object()).sparse(),
+    labels: Joi.array().items(Joi.string().allow('')),
+    routes: Joi.array().items(Joi.object()),
     plugins: Joi.object().unknown(true).pattern(/.+/, Joi.object().keys({
       name: Joi.string(),
       version: Joi.string(),
